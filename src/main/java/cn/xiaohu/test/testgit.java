ba17ca90f628f76a626c094df1bea1a7c37e3fc8
xiaohu <296564447@qq.com>
2018-02-23 02:53:01 +0800

    first commit

---
+++ src/main/java/cn/xiaohu/test/testgit.java
@@ -0,0 +1,9 @@
+package cn.xiaohu.test;
+
+public class testgit
+{
+                public static void main(String [] args)
+                {
+                    System.out.print("hello git!");
+                }
+}
