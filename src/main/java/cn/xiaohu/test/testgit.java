2b60c75ffe364adf48cef94392e989ec3bcbda56
xiaohu111 <33363097+xiaohu111@users.noreply.github.com>
2018-02-23 03:11:41 +0800

    Update testgit.java

--- src/main/java/cn/xiaohu/test/testgit.java
+++ src/main/java/cn/xiaohu/test/testgit.java
@@ -5,5 +5,6 @@ public class testgit
                 public static void main(String [] args)
                 {
                     System.out.print("hello git!");
+                     int i=10;
                 }
 }
